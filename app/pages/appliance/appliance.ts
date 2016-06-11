e85d41fe72958b644a1ecf44cf0c024b55e9f017
chrono <chrono@open-resource.org>
2016-06-11 10:41:16 +0000

    Further appliance page cleanup

--- app/pages/appliance/appliance.ts
+++ app/pages/appliance/appliance.ts
@@ -13,8 +13,188 @@ import {Truncate} from '../../pipes/truncate';
 export class AppliancePage {
 
   private appliance;
+  testRadioOpen: boolean;
+  testRadioResult;
 
   constructor(public nav: NavController) {
+
+/*
+    this.appliance = {
+      'name': 'Digestorium',
+      'desc': 'A fully featured fume hood',
+      'modules': [
+
+        // PID Temperature Governor
+
+        {
+          'name': 'PID Temperature Governor',
+          'icon': 'thermometer',
+          'control': {
+            'name': 'PID Temp Full',
+            'icon': 'thermometer',
+            'driver': 'drv/ctrl-pid-temp',
+            'inputs': [
+              {
+                'type': 'float',
+                'unit': '°C'
+              }
+            ],
+            'outputs': [
+              {
+                'type': 'binary'
+              },
+              {
+                'type': 'binary'
+              }
+            ],
+            'conf': [
+              {
+                'name': 'Max tRun',
+                'val': '300s'
+              },
+              {
+                'name': 'Max °C',
+                'val': '250'
+              },
+              {
+                'name': 'Min °C',
+                'val': '0'
+              },
+              {
+                'name': 'Kp',
+                'val': '30.2'
+              },
+              {
+                'name': 'Ki',
+                'val': '0.77'
+              },
+              {
+                'name': 'Kd',
+                'val': '30.2'
+              }
+            ]
+          },
+          'inputs': [
+            {
+              'name': 'K-Type Probe',
+              'color': '#c70000',
+              'icon': 'thermometer',
+              'type': 'float',
+              'driver': 'drv/input-MAX31855'
+            }
+          ],
+          'outputs': [
+            {
+              'name': 'Heater AC-SSR',
+              'color': 'rgba(255, 153, 0, 0.7)',
+              'icon': 'flame',
+              'type': 'binary',
+              'driver': 'drv/input-gpio',
+              'conf': [
+                {
+                  'name': 'PinIO',
+                  'val': '27'
+                }
+              ]
+            },
+            {
+              'name': 'Cooler AC-SSR',
+              'color': '#0000c7',
+              'icon': 'snow',
+              'type': 'binary',
+              'driver': 'drv/output-gpio',
+              'conf': [
+                {
+                  'name': 'PinIO',
+                  'val': '23'
+                }
+              ]
+            }
+          ]
+        },
+
+        // Magnetic Stir
+
+        {
+          'name': 'Magnetic Stir',
+          'icon': 'nuclear',
+          'control': {
+            'name': 'OnOff',
+            'icon': 'power',
+            'driver': 'drv/ctrl-onoff',
+            'inputs': [{}],
+            'outputs': [
+              {
+                'type': 'binary'
+              }
+            ],
+            'conf': [
+              {
+                'name': 'Max tRun',
+                'val': '300s'
+              }
+            ]
+          },
+          'outputs': [
+            {
+              'name': 'SSR-Circulator',
+              'color': 'purple',
+              'icon': 'nuclear',
+              'type': 'binary',
+              'driver': 'drv/gpio-out',
+              'conf': [
+                {
+                  'name': 'PinIO',
+                  'val': '23'
+                }
+              ]
+            }
+          ]
+        },
+
+        // Door Sensor
+
+        {
+          'name': 'Door',
+          'icon': 'exit',
+          'control': {
+            'name': 'Trigger',
+            'icon': 'flash',
+            'driver': 'drv/ctrl-trigger',
+            'inputs': [
+              {
+                'type': 'Interrupt'
+              }
+            ],
+            'outputs': [{}],
+            'conf': [
+              {
+                'name': 'Action',
+                'val': 'TBD'
+              }
+            ]
+          },
+          'inputs': [
+            {
+              'name': 'Door-Sensor',
+              'color': 'yellow',
+              'icon': 'alert',
+              'type': 'Interrupt',
+              'driver': 'drv/gpio-in',
+              'conf': [
+                {
+                  'name': 'PinIO',
+                  'val': '21'
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  }
+*/
+
 
     this.appliance = {
 
@@ -120,7 +300,7 @@ export class AppliancePage {
             'name': 'OnOff',
             'icon': 'power',
             'driver': 'drv/ctrl-onoff',
-            'inputs': [{}],
+            'inputs': [],
             'outputs': [
               {
                 'type': 'binary'
@@ -187,6 +367,13 @@ export class AppliancePage {
               ]
             }
           ]
+        },
+        {
+          'name': 'RPC - Abzug',
+          'icon': 'exit',
+          'control': {},
+          'inputs': [],
+          'outputs': []
         }
       ]
     }
@@ -275,8 +462,31 @@ export class AppliancePage {
     this.nav.present(prompt);
   }
 
-}
+doRadio() {
+    let alert = Alert.create();
+    alert.setTitle('Lightsaber color');
 
+    alert.addInput({
+      type: 'radio',
+      label: 'Blue',
+      value: 'blue',
+      checked: true
+    });
+
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'OK',
+      handler: data => {
+          console.log('Radio data:', data);
+        this.testRadioOpen = false;
+        this.testRadioResult = data;
+      }
+    });
+   this.nav.present(alert).then(() => {
+      this.testRadioOpen = true;
+    });
+  }
+}
 
 @Page({
   templateUrl: './build/pages/hw-sys/hw-sys-modal.html'
