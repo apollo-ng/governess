c6ef582923e31c1cbfb1299da3b9c30ce70ee475
chrono <chrono@open-resource.org>
2016-08-10 06:39:41 +0000

    Introducing Alerts to protect vital areas from accidental action

--- governess-app/app/pages/settings/settings.ts
+++ governess-app/app/pages/settings/settings.ts
@@ -7,7 +7,8 @@ import { Component }        from '@angular/core';
 import { NavController,
          ViewController,
          ModalController,
-         ToastController }  from 'ionic-angular';
+         ToastController,
+         AlertController }  from 'ionic-angular';
 import { ConfigService }    from '../../providers/config/config';
 import { TaskService }      from '../../providers/tasks/tasks';
 
@@ -29,6 +30,7 @@ export class SettingsPage {
   private nav:              NavController;
   public toastCtrl:         ToastController;
   public modalCtrl:         ModalController;
+  public alertCtrl:         AlertController;
   public taskService:       TaskService;
   public configService:     ConfigService;
   public config:            Object;
@@ -39,12 +41,14 @@ export class SettingsPage {
     nav:                    NavController,
     toastCtrl:              ToastController,
     modalCtrl:              ModalController,
+    alertCtrl:              AlertController,
     configService:          ConfigService,
     taskService:            TaskService
   ) {
     this.nav =              nav;
     this.toastCtrl =        toastCtrl;
     this.modalCtrl =        modalCtrl;
+    this.alertCtrl =        alertCtrl;
     this.configService =    configService;
     this.taskService =      taskService;
     this.config =           this.configService.get();
@@ -65,15 +69,34 @@ export class SettingsPage {
 
   public resetCPD(e: any): void {
 
-    this.configService.reset();
-    this.taskService.reset();
+    let confirm: any = this.alertCtrl.create({
+      title: 'Please confirm:',
+      message: 'Do you really want me to wipe all of your custom settings?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          handler: () => {
+            // console.log('resetCPD aborted');
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.configService.reset();
+            this.taskService.reset();
 
-    let toast: any = this.toastCtrl.create({
-      message: 'GovernessApp configuration has been reset',
-      duration: 3000,
+            let toast: any = this.toastCtrl.create({
+              message: 'GovernessApp configuration has been reset',
+              duration: 3000,
+            });
+
+            toast.present(toast);
+          }
+        }
+      ]
     });
-
-    toast.present(toast);
+    confirm.present();
   }
 
 }
