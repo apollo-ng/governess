98dd82605fe52b6138c3d54203a072c38bf887eb
chrono <chrono@open-resource.org>
2017-02-08 22:30:47 +0000

    fix(client): More chart component refactoring

--- client/src/components/chart/chart.component.ts
+++ client/src/components/chart/chart.component.ts
@@ -1,35 +1,54 @@
-import { Component, Input, ElementRef, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import Chart from 'chart.js';
+
+import { Component,
+         Input,
+         ElementRef,
+         OnInit,
+         OnChanges,
+         SimpleChanges } from '@angular/core';
+
+////////////////////////////////////////////////////////////////////////////////
 
 @Component({
   selector: 'chart',
   template: '<canvas></canvas>',
-  styles: [':host { display: block; }']
+  styles: [':host { display: block; }'],
 })
-export class ChartComponent implements OnInit, OnChanges  {
-  chart: any;
 
-  @Input() type: string;
-  @Input() data: any;
-  @Input() options: any;
+/*******************************************************************************
+ *
+ *   ChartComponent
+ *
+ */
+
+export class ChartComponent implements OnInit, OnChanges {
+
+  public chart: any;
+
+  @Input() public type: string;
+  @Input() public data: any;
+  @Input() public options: any;
 
   constructor(private elementRef: ElementRef) { }
 
-  ngOnInit() {
-    this.chart = new Chart(this.elementRef.nativeElement.querySelector('canvas'), {
-      type: this.type,
-      data: this.data,
-      options: this.options
-    });
+  public ngOnInit(): void {
+    this.chart = new Chart(
+      this.elementRef.nativeElement.querySelector('canvas'), {
+        type: this.type,
+        data: this.data,
+        options: this.options,
+      }
+    );
   }
 
-  ngOnChanges(changes: SimpleChanges) {
+  public ngOnChanges(changes: SimpleChanges): void {
     if (this.chart && changes['data']) {
-      let currentValue = changes['data'].currentValue;
+      let currentValue: any = changes['data'].currentValue;
       ['datasets', 'labels', 'xLabels', 'yLabels'].forEach(property => {
         this.chart.data[property] = currentValue[property];
-      })
+      });
       this.chart.update();
     }
   }
+
 }
--- client/src/components/chart/index.ts
+++ client/src/components/chart/index.ts
@@ -1,2 +1,19 @@
+import { NgModule } from '@angular/core';
+import { ChartComponent } from './chart.component';
+
+////////////////////////////////////////////////////////////////////////////////
+
+/*******************************************************************************
+ *
+ *   NgModule for Chart
+ *
+ */
+
+@NgModule({
+  declarations: [ ChartComponent ],
+  exports: [ ChartComponent ],
+})
+
+export class ChartModule {}
+
 export * from './chart.component';
-export * from './chart.module';
